c855e170e325aa34adbff82baf7fd883092cedc9
Marc Hermon <hermonm@urbandaleschools.com>
2019-03-03 12:13:05 -0600

    Update 12.0_Take_Home_Test.py

--- 12.0_Take_Home_Test.py
+++ 12.0_Take_Home_Test.py
@@ -1,71 +1,49 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
 but I will definitely not ask another person except the instructor. Signed: ______________________
-
-1.) Correct the following code: (The user's number should be increased by 1 and printed.)
-'''
-def increase(x):
-    return x + 1
  
-num = input("Enter a number: ")
-increase(x)
-print("Your number has been increased to", x)
-                        
- 
+ 1. Write code that defines a class named Animal:
+     * Add a constructor for the Animal class that prints 'An animal has been born.'
+     * Add an attribute for the animal name.
+     * Add an eat() method for Animal that prints 'Munch munch.'
+     * Add a make_noise() method for Animal that prints 'Grrr says [animal name].'
 
+     
+ 2. Write code that defines a class named Cat:
+     * Make Animal the parent.
+     * Add a constructor for Cat that prints 'A cat has been born.'
+     * Modify the constructor so it calls the parent constructor as well.
+     * Add a make_noise() method for Cat that prints 'Meow says [animal name].'
+
+     
+ 3. Write code that defines a class named Dog:
+     
+     * Make Animal the parent.
+     * Add a constructor for Dog that prints 'A dog has been born.'
+     * Modify the constructor so it calls the parent constructor as well.
+     * Add a make_noise() method for Dog that prints 'Bark says [animal name].'
+
+     
+ 4. Write a main program with:
+     
+     * Code that creates a cat, two dogs, and an animal with names.
+     * Code that calls eat() and make_noise() for each animal.
+     
+ OUTPUT:
+An animal has been born.
+A cat has been born.
+Munch munch
+Meow says (cat name) .
+An animal has been born.
+A dog has been born.
+Munch munch
+Bark says (dog name).
+An animal has been born.
+A dog has been born.
+Munch munch
+Bark says (another dog name) .
+An animal has been born.
+Munch munch
+Grrr says (animal name) .
 '''
-2.) Correct the following code to print 1-10:
-'''
-def count_to_ten:
-    for i in range[10]:
-        print(i)
- 
-count_to_ten()
 
-
-
-'''
-3.) Correct the following code to sum the list:
-'''
-def sum_list(list):
-    for i in list:
-        sum = i
-        return sum
- 
-list = [45, 2, 10, -5, 100]
-print(sum_list(list))
-
-
-
-'''
-4.) Correct the following code which should reverse the sentence that is entered.
-'''
-def reverse(text):
-    result = ""
-    text_length = len(text)
-    for i in range(text_length):
-        result = result + text[i * -1]
-    return result
- 
-text = input("Enter a sentence: ")
-print(reverse(text))
-
-
-'''
-5.) Correct the following code: (if one of the options is not entered it should print the statements)
-'''
-def get_user_choice():
-    while True:
-        command = input("Command: ")
-        if command = f or command = m or command = s or command = d or command = q:
-            return command
- 
-        print("Hey, that's not a command. Here are your options:" )
-        print("f - Full speed ahead")
-        print("m - Moderate speed")
-        print("s - Status")
-        print("d - Drink")
-        print("q - Quit")
- 
-user_command = get_user_choice()
-print("You entered:", user_command)
